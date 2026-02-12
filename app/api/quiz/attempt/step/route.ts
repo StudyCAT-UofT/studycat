1bffcfa48f55cd447d9b17468b9a9319225798cc
jchionak <jchionak@icloud.com>
2026-02-12 01:02:32 -0500

    check if all modules have been mastered

--- app/api/quiz/attempt/step/route.ts
+++ app/api/quiz/attempt/step/route.ts
@@ -119,8 +119,10 @@ export const POST = async (request: NextRequest) => {
       );
     }
 
+    console.log(fastApiData.next_action)
+
     // Update attempt based on response
-    if (fastApiData.next_action === 'FINISH') {
+    if (fastApiData.next_action === 'FINISH' || fastApiData.next_action === 'MASTERED') {
       await prisma.attempt.update({
         where: { id: attempt.id },
         data: {
@@ -148,6 +150,7 @@ export const POST = async (request: NextRequest) => {
       nextAction: fastApiData.next_action,
       nextItem: fastApiData.next_item,
       isFinished: fastApiData.next_action === 'FINISH',
+      allMastered: fastApiData.next_action === 'MASTERED',
       feedback: {
         correctAnswerIndex: correctAnswerIndex,
         selectedAnswerIndex: answerIndex,
--- types/index.ts
+++ types/index.ts
@@ -102,6 +102,7 @@ export interface StepAttemptResponse {
     nextAction: string
     nextItem?: QuizItem
     isFinished: boolean
+    allMastered: boolean
     feedback?: Feedback
 }
 
