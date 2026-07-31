715e0e1b6b4d1bb0e069f7a4af94610903583e26
elena-ding <emailelenading@gmail.com>
2026-07-31 09:18:56 -0400

    Remove biserial from is a valid number check so values like 'N/A' are allowed

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -167,7 +167,6 @@ export async function POST(request: Request) {
                 }
 
                 const hasInvalidNumbers = 
-                    Number.isNaN(ptBi) ||
                     Number.isNaN(average) ||
                     Number.isNaN(attemptsCount) ||
                     Number.isNaN(irtA) || 
